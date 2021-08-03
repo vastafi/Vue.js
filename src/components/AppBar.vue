287e6e052477afc20e817c11e2379b76529d26bc
Astafi Valentina <vastafi@pentalog.com>
2021-08-03 18:43:38 +0300

    Add avatar

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -5,7 +5,7 @@
       flat
   >
     <v-container class="py-0 fill-height">
-            <v-btn
+      <v-btn
           v-for="link in links"
           :key="link"
           text
@@ -25,11 +25,35 @@
         ></v-text-field>
       </v-responsive>
     </v-container>
-    <v-avatar
-        class="mr-10"
-        color="grey darken-1"
-        size="32"
-    ></v-avatar>
+    <v-menu offset-y>
+      <template v-slot:activator="{ on, attrs }">
+        <v-badge
+            bordered
+            bottom
+            color="deep-purple accent-4"
+            offset-x="10"
+            offset-y="10"
+            :left="true"
+            content="5"
+        >
+          <v-avatar size="40"
+                    v-bind="attrs"
+                    v-on="on">
+            <v-img src="https://cdn.vuetifyjs.com/images/lists/2.jpg"></v-img>
+
+          </v-avatar>
+        </v-badge>
+
+      </template>
+      <v-list>
+        <v-list-item
+            v-for="(item, index) in items"
+            :key="index"
+        >
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
   </v-app-bar>
 </template>
 
@@ -42,6 +66,12 @@ export default {
       'Contacts',
       'About',
       'Updates',
+    ],
+    items: [
+      {title: 'View account'},
+      {title: 'Cart'},
+      {title: 'Settings'},
+      {title: 'Log Out'},
     ],
   }),
 }
