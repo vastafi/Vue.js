110ae12168203938dece3760143b8cad3c13f01b
Astafi Valentina <vastafi@pentalog.com>
2021-08-03 18:45:39 +0300

    Correcting main,js

--- src/main.js
+++ src/main.js
@@ -2,18 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 
-/* example import */
-
-// import {NAME} from ".importTest";
-
-let TEST = require('./importTest');
-console.log(TEST);
-
-(function (test) {
-    let TEST = require('./importTest');
-    console.log(test + TEST.TEST);
-})("Hello_")
-
 Vue.config.productionTip = false
 
 new Vue({
