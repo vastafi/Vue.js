4852f9f316a2ce619ad05bfbcbaf4de498417e69
Astafi Valentina <vastafi@pentalog.com>
2021-08-03 18:11:49 +0300

    Add content page

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,34 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+  <v-app id="inspire">
+    <AppBar></AppBar>
+    <v-main class="grey lighten-3">
+      <v-container>
+        <v-row>
+          <v-col cols="2">
+            <CategoryList></CategoryList>
+                     </v-col>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
+          <v-col>
+            <v-sheet
+                min-height="70vh"
+                rounded="lg"
+            >
+              <Content></Content>
+            </v-sheet>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import Content from "./components/Content";
+import AppBar from "./components/AppBar";
+import CategoryList from "./components/CategoryList";
 
 export default {
-  name: 'App',
+  components: {CategoryList, AppBar, Content},
 
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
-};
+}
 </script>
---
+++ src/components/AppBar.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-app-bar
+      app
+      color="white"
+      flat
+  >
+    <v-container class="py-0 fill-height">
+            <v-btn
+          v-for="link in links"
+          :key="link"
+          text
+      >
+        {{ link }}
+      </v-btn>
+
+      <v-spacer></v-spacer>
+
+      <v-responsive max-width="260">
+        <v-text-field
+            dense
+            flat
+            hide-details
+            rounded
+            solo-inverted
+        ></v-text-field>
+      </v-responsive>
+    </v-container>
+    <v-avatar
+        class="mr-10"
+        color="grey darken-1"
+        size="32"
+    ></v-avatar>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "AppBar",
+  data: () => ({
+    links: [
+      'Products',
+      'Contacts',
+      'About',
+      'Updates',
+    ],
+  }),
+}
+</script>
+
+<style scoped>
+
+</style>
