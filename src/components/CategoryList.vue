db3466b215082206fbf45e6e86e3832b2b3d59e0
Astafi Valentina <vastafi@pentalog.com>
2021-08-05 13:34:43 +0300

    added categories and subcategories from api

--- src/components/CategoryList.vue
+++ src/components/CategoryList.vue
@@ -2,8 +2,7 @@
   <v-sheet rounded="lg">
     <v-list color="transparent">
       <v-list-item
-        link
-        color="grey lighten-4"
+          color="grey lighten-4"
       >
         <v-list-item-content>
           <v-list-item-title>
@@ -12,18 +11,33 @@
         </v-list-item-content>
       </v-list-item>
 
-      <v-divider class="my-3" />
-      <v-list-item
-
-        v-for="(item, index) in $store.getters['categories/getList']"
-        :key="index"
-      >
-        <v-list-item-content>
-          <v-list-item-title>
-            {{ item.name }}
-          </v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
+      <v-divider class="my-3"/>
+      <v-expansion-panels focusable>
+        <v-expansion-panel
+            v-for="link in $store.getters['categories/getList']"
+            :key="link"
+        >
+          <div v-if="!link.parentLink">
+            <v-expansion-panel-header>{{ link.name }}</v-expansion-panel-header>
+            <div
+                v-for="linkChild in $store.getters['categories/getList']"
+                :key="linkChild"
+            >
+              <v-expansion-panel-content
+                  v-if="linkChild.parentLink === link.link"
+              >
+                <v-list-item>
+                  <v-list-item-content>
+                    <v-list-item-title class="text-wrap">
+                      {{ linkChild.name }}
+                    </v-list-item-title>
+                  </v-list-item-content>
+                </v-list-item>
+              </v-expansion-panel-content>
+            </div>
+          </div>
+        </v-expansion-panel>
+      </v-expansion-panels>
     </v-list>
   </v-sheet>
 </template>
@@ -32,14 +46,14 @@
 export default {
   name: "CategoryList",
   data: () => ({
-    items: [
-      {title: 'Phones'},
-      {title: 'Notebooks'},
-      {title: 'Printers'},
+    links: [
+      'Phones',
+      'Notebooks',
+      'Printers',
     ],
   }),
   mounted() {
-    this.$store.dispatch('categories/fetch')
+    this.$store.dispatch('categories/fetch');
   }
 }
 </script>
