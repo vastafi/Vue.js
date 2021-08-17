623d9154d2e9755514c1a61ca41b7cc6c719a60b
Astafi Valentina <vastafi@pentalog.com>
2021-08-17 23:34:38 +0300

    Add link to product details

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -1,20 +1,51 @@
 <template>
-  <v-main>
-    <v-container>
-      {{ id }}
-    </v-container>
-  </v-main>
+  <v-sheet
+    rounded="lg"
+    min-height="268"
+  >
+    <div
+      v-if="$store.getters['item/getIsLoading']"
+    />
+    <div
+      v-else
+    >
+      <v-carousel>
+        <v-carousel-item
+          v-for="(image, i) in $store.getters['item/getItem'].img"
+          :key="i"
+          :src="image"
+          reverse-transition="fade-transitions"
+          transition="fade-transition"
+        />
+      </v-carousel>
+    </div>
+  </v-sheet>
 </template>
 
 <script>
+import {mapGetters, mapActions} from 'vuex'
+
 export default {
   name: "Item",
-  props:{
-    id:{
+  props: {
+    id: {
       type: String,
       required: true,
-    }
-  }
+    },
+  },
+  mounted() {
+    this.loadItem (this.id)
+  },
+  computed: {
+    ...mapGetters({
+      item: 'item/getItem'
+    })
+  },
+  methods: {
+    ...mapActions({
+      loadItem: 'item/loadItem'
+    })
+  },
 }
 </script>
 
--- src/store/modules/item.js
+++ src/store/modules/item.js
@@ -1,20 +1,21 @@
 export default{
-    state:{
+    state: {
         item:{},
         isLoading:false,
     },
-    getters:{
-        getItem:(item) => item,
+    getters: {
+        getItem: ({item}) => item,
+        getIsLoading: (state) => state.isLoading,
     },
-    actions:{
-       async loadItem(store, payload){
+    actions: {
+       async loadItem(store, payload) {
            store.commit('mutateIsLoading', true);
-           let result = await fetch(`/api/item?query=${payload}`);
-           store.commit('mutateItem', await result.json());
+           const item = await fetch(`/api/item?link=/ru/${payload}`);
+           store.commit('mutateItem', await item.json());
            store.commit('mutateIsLoading', false);
        }
     },
-    mutation:{
+    mutation: {
         mutateIsLoading(state, payload) {
             state.isLoading = payload;
         },
