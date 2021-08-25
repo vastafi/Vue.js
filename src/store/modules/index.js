ed831063585a21090a6b5857298a0c64cffe5dbe
Astafi Valentina <vastafi@pentalog.com>
2021-08-25 20:44:20 +0300

    replaced fetch with axios requests

--- src/store/modules/index.js
+++ src/store/modules/index.js
@@ -2,4 +2,4 @@ export {default as categories} from './categories';
 export {default as products} from './products';
 export {default as settings} from './settings';
 export {default as item} from './item';
-export {default as history} from './productHistory';
+export {default as productHistory} from './productHistory';
